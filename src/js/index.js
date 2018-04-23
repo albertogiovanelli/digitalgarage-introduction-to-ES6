3cfbf5437982a676b8307408c011b8d8f0ca459b
alberto.giovanelli <alberto.giovanelli@i-seed.it>
2018-04-23 10:11:02 +0200

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+console.log("Hello World!");
+
+const controlLogin = () => {
+
+};
+
+document.querySelector('.login__container').addEventListener('submit', e => {
+    e.preventDefault();
+    controlLogin();
+});
